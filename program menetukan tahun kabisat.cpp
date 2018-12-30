dfc04cc87ad026c7aad44f0221a103dfbb69555a
Nursetiana Situmorang <45727455+nursetiana09@users.noreply.github.com>
2018-12-30 12:32:02 +0700

    Add files via upload

---
+++ program menetukan tahun kabisat.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main ()
+{
+    cout<< "menentukan tahun kabisat "<<endl<<endl;
+    int thn;
+    cout<<"masukan tahun = ";cin>>thn;
+
+ if ((thn^4==0) || (thn%100==0));
+ if ((thn%400!=0))
+
+ cout<<" tahun kabisat"<<endl;
+ else
+     cout<<" bukan tahun kabisat"<<endl;
+ getch();
+}
